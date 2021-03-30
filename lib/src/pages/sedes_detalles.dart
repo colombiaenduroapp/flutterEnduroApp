70304ea9a58bf345c56a3fe81e4c194f4a1cfa11
jepore97 <jepore97@gmail.com>
2021-03-30 00:12:25 -0500

    cambio socket registro usuario

--- lib/src/pages/sedes_detalles.dart
+++ lib/src/pages/sedes_detalles.dart
@@ -589,6 +589,7 @@ class _page_sedes_detallesState extends State<page_sedes_detalles> {
             bool res = false;
             WidgetsGenericos.showLoaderDialog(
                 context, true, 'Cargando...', null, Colors.blue);
+            print(widget.data);
             res = await ServicioSede().deleteMesa(widget.data);
             if (res) {
               Navigator.pop(context);
